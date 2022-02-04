339df8bb65ddaf1b0743c9322e44aca904757660
romoho <roman.saviuk.knm.2020@lpnu.ua>
2022-02-04 02:57:09 +0200

    home ready

--- ClientApp/src/components/Home.jsx
+++ ClientApp/src/components/Home.jsx
@@ -1,5 +1,5 @@
 ﻿import React, { Component } from 'react';
-import { Container, Row, Col, Input, Button} from 'reactstrap';
+import { Container, Row, Col, Input} from 'reactstrap';
 import './Home.scss';
 
 export class Home extends Component {
@@ -7,18 +7,18 @@ export class Home extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { id: '' };
+        this.state = { idQ: '' };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ id: event.target.id });
+        this.setState({ idQ: event.target.value });
     }
 
     handleSubmit(event) {
-        alert('An id was submitted: ' + this.state.id);
+        alert('An id was submitted: ' + this.state.idQ);
         event.preventDefault();
     }
 
@@ -27,19 +27,24 @@ export class Home extends Component {
             <Container fluid>
                 <Row>
                     <Col sm="6">
-                        <div className="input_background_background">
-                            <div className="input_background">
-                                <Input value={this.state.value} onChange={this.handleChange} id="idQ" placeholder="ID..."/>
+                        <div className="input_id_block">
+                            <div className="input_background_background">
+                                <div className="input_background">
+                                    <Input type="text" value={this.state.idQ} onChange={this.handleChange} placeholder="ID..."/>
+                                </div>
                             </div>
-                        </div>
 
-                        <div>
-                            <input type="submit" value="Submit" />
+                            <div className="submit_button">
+                                <Input style={{color:"white"}} type="submit" value="Join" onClick={this.handleSubmit} />
+                            </div>
                         </div>
                     </Col>
 
                     <Col sm="6">
-                        Stop wasting time in queues use smart queue service today
+                        <div className="about_block">
+                            <h1>Stop wasting time in queues</h1>
+                            <h2>use smart queue service today</h2>
+                        </div>
                     </Col>
                 </Row>
             </Container>
